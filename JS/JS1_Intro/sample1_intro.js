3460f843282bc243359144bdb732de5cc28cbd6d
vmurali100 <vmurali100@gmail.com>
2024-07-30 08:30:37 +0530

    Added Functions

---
+++ JS/JS1_Intro/sample1_intro.js
@@ -0,0 +1,17 @@
+document.getElementById("demo").style.color = "red";
+// alert("Welcome to javascript Alerts ....");
+// document.write("Welcome to javascript  Document write Method !!")
+// document.getElementById("myPara").innerHTML = "Welcome to javascript ";
+// console.log("Welcome to javascript  !!!!");
+
+var myName = "Theju"; // String will always Wrapped in "";
+var myAge = 20;
+var areYouInChittoor = true;
+var yourSalary = null;
+var yourRole;
+
+// alert("Welcome to Javascript !!")
+// var myName = prompt("Enter Your Name ");
+// document.write(myName);
+
+confirm('Are you Interest TO become Full Stack Developer ?')
